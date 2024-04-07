4b8dfdff0cc56749264b490c46e3f44fa9cbcef8
Leroy Salih <leroy.salih@gmail.com>
2024-04-07 06:16:55 +0100

    Build

--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,26 @@ import { DateTime } from "luxon";
 
 import {getUser, getProfile} from "../lib/server";
 
-
-
 const MainPage = async () => {
 
-  const supabase = createSupabaseServerClient(false);
+  try {
+
+    const supabase = createSupabaseServerClient(false);
   
   if (!supabase) {
-    return <h1>Error: Creating supabase. </h1>
+    throw new Error("Supbase Server Client didn't return an object.")
   }
 
   let user = await getUser(0);
 
+  // if the first call to getUser
   if (user === null){
+    // if the first call to getUser returns null, try again - it may be that the server needs time to catch up.
     user = await getUser(1000);
   }
 
-
-
   if (user !== null) {
-
+    // if a user has been returned, get the profile.
     const profile = await getProfile(user.id);
 
     //redirect(`/app/spec-report/${user.id}`);
@@ -34,29 +34,22 @@ const MainPage = async () => {
       redirect('/app/new-profile')
     }
     
+    // display the dashboard for this user
     return <Dashboard profile={profile}/>
 
   } else {
+    // display the signin page
     return <>
-    <SignIn />
-    
+      <SignIn />
     </>
   }
-
+  } catch(error : any) {
+    console.error(DateTime.now(), error);
+    return <>
+      <h1>An error occured</h1>
+      <pre>{JSON.stringify(error.message, null, 2)}</pre>
+    </>
+  }
 }
 
 export default MainPage;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
